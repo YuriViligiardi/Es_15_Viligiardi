f71d6dbf266a2cd2563cecce81a6574ac896b14c
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-04 08:33:04 +0200

    Aggiunte funzioni createType, createLista e stampaLista

--- Es_StampaList.php
+++ Es_StampaList.php
@@ -7,6 +7,38 @@
 </head>
 <body>
     <?php
+        $numero = rand(1,50);
+        $numCasuale = rand(1,3);
+        $tipo = createType($numCasuale);
+        stampaLista($numero, $tipo);
+
+        function createType($nc){
+            if($nc == 1){
+                return "o";
+            } else {
+                return "u";
+            }
+        }
+
+        function stampaLista($num, $t){
+            if($t == 'o'){
+                echo "<ol>";
+                createLista($num);
+                echo "</ol>";
+            } else {
+                echo "<ul>";
+                createLista($num);
+                echo "</ul>";
+            }
+        }
+
+        function createLista($n){
+            for ($i=0; $i < $n; $i++) { 
+                if (($i % 2 == 0) || ($i % 5 == 0)) {
+                    echo "<li>$i</li>";
+                }
+            }
+        }
     ?>
 </body>
 </html>
